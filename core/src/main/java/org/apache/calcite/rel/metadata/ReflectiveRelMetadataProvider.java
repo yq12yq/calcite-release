64101f2631f567d821a133488e965cde4894de6e
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2016-01-13 11:36:19 +0300

    [CALCITE-1053] CPU spin in ReflectiveRelMetadataProvider.apply -> HashMap.get
    The map in question is shared between multiple threads and updated at the same time.
    Thus the map should be a ConcurrentMap.
    
    Problem was reported here: https://mail-archives.apache.org/mod_mbox/calcite-dev/201601.mbox/%3C56952E8F.7090705%40gmail.com%3E

--- core/src/main/java/org/apache/calcite/rel/metadata/ReflectiveRelMetadataProvider.java
+++ core/src/main/java/org/apache/calcite/rel/metadata/ReflectiveRelMetadataProvider.java
@@ -37,6 +37,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * Implementation of the {@link RelMetadataProvider} interface that dispatches
@@ -54,7 +56,7 @@ public class ReflectiveRelMetadataProvider
     implements RelMetadataProvider, ReflectiveVisitor {
 
   //~ Instance fields --------------------------------------------------------
-  private final Map<Class<RelNode>, Function<RelNode, Metadata>> map;
+  private final ConcurrentMap<Class<RelNode>, Function<RelNode, Metadata>> map;
   private final Class<?> metadataClass0;
 
   //~ Constructors -----------------------------------------------------------
@@ -66,7 +68,7 @@ public class ReflectiveRelMetadataProvider
    * @param metadataClass0 Metadata class
    */
   protected ReflectiveRelMetadataProvider(
-      Map<Class<RelNode>, Function<RelNode, Metadata>> map,
+      ConcurrentMap<Class<RelNode>, Function<RelNode, Metadata>> map,
       Class<?> metadataClass0) {
     assert !map.isEmpty() : "are your methods named wrong?";
     this.map = map;
@@ -128,8 +130,12 @@ public class ReflectiveRelMetadataProvider
       }
     }
 
-    final Map<Class<RelNode>, Function<RelNode, Metadata>> methodsMap =
-        Maps.newHashMap();
+    // This needs to be a councurrent map since RelMetadataProvider are cached in static
+    // fields, thus the map is subject to concurrent modifications later.
+    // See map.put in org.apache.calcite.rel.metadata.ReflectiveRelMetadataProvider.apply(
+    // java.lang.Class<? extends org.apache.calcite.rel.RelNode>)
+    final ConcurrentMap<Class<RelNode>, Function<RelNode, Metadata>> methodsMap =
+        new ConcurrentHashMap<>();
     for (Class<RelNode> key : classes) {
       ImmutableNullableList.Builder<Method> builder =
           ImmutableNullableList.builder();
