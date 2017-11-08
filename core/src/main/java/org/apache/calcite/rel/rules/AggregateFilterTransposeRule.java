eb6d94ae673574c542a757a4d2a874f0362d5431
Volodymyr Vysotskyi <vvovyk@gmail.com>
2017-11-08 11:12:51 +0000

    [CALCITE-2043] Use custom RelBuilder implementation in some rules (Volodymyr Vysotskyi)
    Close apache/calcite#564
    
    (cherry picked from commit d407395e05b5b37e019a44f812aae04540e6f398)
    
    Change-Id: I64853d8fab9842d737b485b9b699d3b0e8f43738

--- core/src/main/java/org/apache/calcite/rel/rules/AggregateFilterTransposeRule.java
+++ core/src/main/java/org/apache/calcite/rel/rules/AggregateFilterTransposeRule.java
@@ -67,7 +67,8 @@ public class AggregateFilterTransposeRule extends RelOptRule {
         RelFactories.LOGICAL_BUILDER);
   }
 
-  protected AggregateFilterTransposeRule(RelOptRuleOperand operand,
+  /** Creates an AggregateFilterTransposeRule. */
+  public AggregateFilterTransposeRule(RelOptRuleOperand operand,
       RelBuilderFactory relBuilderFactory) {
     super(operand, relBuilderFactory, null);
   }
